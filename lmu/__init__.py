c68e1a9ff0350a7f480a847486d795dd413d3c86
Aaron Voelker <aaron.voelker@appliedbrainresearch.com>
2020-06-09 13:27:50 -0400

    Fixed __version__ and __copyright__

--- lmu/__init__.py
+++ lmu/__init__.py
@@ -1,5 +1,6 @@
 from .lmu import Legendre, InputScaled, LMUCell, LMUCellODE, LMUCellGating
 
-__copyright__ = "2019, Applied Brain Research"
+from .version import version as __version__
+
+__copyright__ = "2019-2020, Applied Brain Research"
 __license__ = "Free for non-commercial use; see LICENSE.rst"
-__version__ = "0.1.0"
