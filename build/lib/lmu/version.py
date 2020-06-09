c5f31bda3b679a1df56e08c9570c6636ebbe3fba
Aaron Voelker <aaron.voelker@appliedbrainresearch.com>
2020-06-09 13:25:46 -0400

    Remove build directory

--- build/lib/lmu/version.py
+++
@@ -1,8 +0,0 @@
-name = "lmu"
-version_info = (0, 1, 0)  # (major, minor, patch)
-dev = 0
-
-version = "{v}{dev}".format(
-    v=".".join(str(v) for v in version_info),
-    dev=(".dev%d" % dev) if dev is not None else "",
-)
